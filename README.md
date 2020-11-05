3d0cdda5643c0eff82c2e05741b6cbd07186baca
gemmi <ivan.gemmet@bucher-suter.com>
2020-11-05 18:38:05 +0100

    Added license file and wrote better instructions in the Readme

--- README.md
+++ README.md
@@ -5,12 +5,12 @@ This project is a sample that shows how to use the Webex SDK with a Webpack 5 pr
 * Login to https://developer.webex.com
 * Create a new Application
 * Add redirect URI(s) https://localhost:8080/index.html
-* Copy the Client ID and paste it into the src/index.ts file
-* install the dependencies with npm install / yarn
-
-## How to run this
+* Copy the Client ID and replace 'YOUR_CLIENT_ID' with it into the src/index.ts file
+* install the dependencies with `npm install` or `yarn`
 * type `npm run start` or `yarn start`
 * open a browser `https://localhost:8080`
 * Login to webex
 * Open the Developer Tools
 * Check the console
+* Login to Webex teams and send a message
+* it will appear in the console
--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ window.setTimeout(() => {
   const webex = ((window as any).webex = initWebex({
     config: {
       credentials: {
-        client_id: 'Ccf162333bbb3d789b69b14a7e7a73f1df58f7a29b6c4a4ba9bb11019d845ab25',
+        client_id: 'YOUR_CLIENT_ID',
         redirect_uri: 'https://localhost:8080/index.html',
         scope: 'spark:all spark:kms'
       }
@@ -25,7 +25,7 @@ window.setTimeout(() => {
     else {
       webex.authorization.initiateLogin(
         {
-          client_id: 'Ccf162333bbb3d789b69b14a7e7a73f1df58f7a29b6c4a4ba9bb11019d845ab25',
+          client_id: 'YOUR_CLIENT_ID',
           redirect_uri: 'https://localhost:8080/index.html',
           scope: 'spark-compliance%3Amemberships_read%20spark-admin%3Aresource_groups_read%20meeting%3Arecordings_read%20spark%3Aall%20meeting%3Apreferences_write%20spark-admin%3Apeople_write%20spark-admin%3Aorganizations_read%20spark-admin%3Aplaces_read%20meeting%3Aschedules_write%20spark-compliance%3Ateam_memberships_read%20spark-compliance%3Ateam_memberships_write%20spark-admin%3Adevices_read%20spark-admin%3Ahybrid_clusters_read%20spark-compliance%3Amessages_read%20spark-admin%3Adevices_write%20meeting%3Aschedules_read%20spark-compliance%3Amemberships_write%20identity%3Aplaceonetimepassword_create%20spark-admin%3Aroles_read%20meeting%3Arecordings_write%20meeting%3Apreferences_read%20spark-admin%3Aresource_group_memberships_read%20spark-compliance%3Aevents_read%20spark-admin%3Aresource_group_memberships_write%20spark-compliance%3Arooms_read%20spark-admin%3Acall_qualities_read%20spark-compliance%3Amessages_write%20spark%3Akms%20spark-admin%3Ahybrid_connectors_read%20audit%3Aevents_read%20spark-compliance%3Ateams_read%20spark-admin%3Aplaces_write%20spark-admin%3Alicenses_read%20spark-admin%3Apeople_read'
         }
